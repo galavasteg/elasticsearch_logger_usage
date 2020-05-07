9b946b7b53e19b90e5a872744832187518cf7c8e
Artem Shakurov <ashakurov@gmcs.ru>
2020-05-07 14:26:59 +0300

    logger usage

--- main.py
+++ main.py
@@ -52,3 +52,27 @@ log_config = {
 
 logging.config.dictConfig(log_config)
 
+
+log = logging.getLogger('main_log')
+
+
+log.info('START')
+
+try:
+    some_result = sum(range(10))
+    log.info(f'1. Немного кириллицы and something'
+             f' use\u0336f\u0336u\u0336l\u0336lless: {some_result}')
+
+    raise RuntimeError('Hey! What happened?!')
+
+except RuntimeError as e:
+    log.exception('3. Your amazing exception record:'
+                  ' "log.exception(\'foo\')"')
+    log.error('4. "log.error(\'bar\', exc_info=True)" is'
+              ' an analog of "log.exception(\'bar\')"',
+              exc_info=True)
+    log.error('5. "log.error(\'baz\', exc_info=False)" not provides any'
+              ' information about the exception. It\'s simple, isn\'t it! =)',
+              exc_info=False)
+
+log.info('FINISH')
